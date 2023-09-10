7bfd72e83f51c71d2ef088c48d67cedf5c93711c
Pranav Agrawal <pranavagrawal321@gmail.com>
2023-09-11 02:31:21 +0530

    added books page

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
+# app.py
+
+from flask import Flask, render_template, request, jsonify
+
 import pandas as pd
-from flask import Flask, render_template, request
 import numpy as np
 
+# Load your data
 popular_df = pd.read_pickle("popular_books.pkl")
 pt = pd.read_pickle("pt.pkl")
 books = pd.read_pickle("books.pkl")
 similarity_scores = pd.read_pickle("similarity_scores.pkl")
 
 app = Flask(__name__)
+
+
+# Function to retrieve book suggestions based on user input
+def get_suggestions(query):
+    # Replace this with your logic to fetch book suggestions
+    # For example, you can search for book titles or authors that match the query
+    suggestions = []
+    for title in books["Book-Title"]:
+        if query.lower() in title.lower():
+            suggestions.append(title)
+    return suggestions
 
 
 @app.route("/")
@@ -35,15 +50,28 @@ def recommend():
     for i in similar_items:
         item = []
         temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        item.extend(temp_df.drop_duplicates('Book-Title')['Book-Title'].to_list())
+        item.extend(temp_df.drop_duplicates('Book-Title')['Book-Author'].to_list())
+        item.extend(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].to_list())
         
         data.append(item)
     
-    print(data)
-    
     return render_template('recommend.html', data=data)
+
+
+@app.route("/books", methods=["GET"])
+def all_books():
+    return render_template("all.html", book_name=books["Book-Title"].to_list(),
+                           author_name=books["Book-Author"].to_list(),
+                           image=books["Image-URL-M"].to_list()
+                           )
+
+
+@app.route('/get_suggestions')
+def suggestions():
+    query = request.args.get('query')
+    suggestions = get_suggestions(query)
+    return jsonify(suggestions)
 
 
 if __name__ == "__main__":
